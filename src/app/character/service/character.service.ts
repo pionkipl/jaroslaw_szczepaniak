219c61052ebefb45790fbb683ad23b57e761160f
pionkipl <jarek491@gmail.com>
2019-11-10 13:06:58 +0100

    Add rest api for character.

--- src/app/character/service/character.service.ts
+++ src/app/character/service/character.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { CharacterModel } from '../model/character-model.interface';
 
@@ -10,8 +10,37 @@ export class CharacterService {
   constructor(private http: HttpClient) {}
   url = 'http://localhost:3000';
 
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  };
+
   getCharacters() {
     return this.http.get<Array<CharacterModel>>(`${this.url}/characters`);
+  }
+
+  createCharacter(character) {
+    return this.http.post<CharacterModel>(
+      `${this.url}/characters`,
+      JSON.stringify(character),
+      this.httpOptions
+    );
+  }
+
+  updateCharacter(id, character) {
+    return this.http.put<CharacterModel>(
+      `${this.url}/characters/${id}`,
+      JSON.stringify(character),
+      this.httpOptions
+    );
+  }
+
+  deleteCharacter(id) {
+    return this.http.delete<CharacterModel>(
+      `${this.url}/characters/${id}`,
+      this.httpOptions
+    );
   }
 
   searchCharacterByQuery(characterSearchQuery: string) {
